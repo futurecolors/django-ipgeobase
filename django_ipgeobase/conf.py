6b9e6433302cd35f23a447787374392c5e33f003
Ilya Baryshev <baryshev@gmail.com>
2011-06-01 10:38:52 +0400

    Импорт данных с ipgeobase.ru в новом формате Старый формат более не поддерживается (с 15 апреля 2011)

--- django_ipgeobase/conf.py
+++ django_ipgeobase/conf.py
@@ -1,31 +1,15 @@
 #encoding: utf-8
-from django.conf import settings
 
-#Путь до файла block_coord.db
-IPGEOBASE_SOURCE_URLS = getattr(settings, 'IPGEOBASE_SOURCE_URL', 'http://ipgeobase.ru/files/db/Map_db/block_coord.zip')
-IPGEOBASE_FILENAME = getattr(settings, 'IPGEOBASE_FILENAME', "block_coord.db")
-IPGEOBASE_CODING = getattr(settings, 'IPGEOBASE_CODING', 'windows-1251')
-IPGEOBASE_SEND_MESSAGE_FOR_ERRORS = getattr(settings, 'IPGEOBASE_SEND_MESSAGE_FOR_ERRORS', True)
+IPGEOBASE_CITIES_INFO = {
+        'url': 'http://ipgeobase.ru/files/db/Main/geo_files.zip',
+        'filename': 'cities.txt',
+        'encoding': 'windows-1251',
+        'fields': ['city_id', 'city_name', 'region_name', 'district', 'lat', 'lng'],
+    }
 
-IPGEOBASE_USE_MULTICOUNTRY = True
-IPGEOBASE_COUNTRY_INFO = [
-    {
-        'url': 'http://ipgeobase.ru/files/db/Main/db_files.zip',
-        'format': 'zip',
-        'zip_filename': 'cidr_ru_block.txt',
-        'fields': ['start_ip', 'end_ip', 'ip_block', None, 'city', 'region', None, ],
-        'coding': 'windows-1251',
-        'defaults':{
-            'country': 'ru',
-            }
-        },
-    {
-        'url': 'http://ipgeobase.ru/files/db/Main/ua_inetnums.txt',
-        'format': 'text',
-        'fields': ['ip_block', 'start_ip', 'end_ip', 'city', 'region'],
-        'coding': 'windows-1251',
-        'defaults': {
-            'country':'ua',
-            }
-        }
-    ]
+IPGEOBASE_CIDR_INFO = {
+        'url': 'http://ipgeobase.ru/files/db/Main/geo_files.zip',
+        'filename': 'cidr_optim.txt',
+        'encoding': 'windows-1251',
+        'fields': ['start_ip', 'end_ip', 'ip_block', 'country_id', 'city_id'],
+    }
--- django_ipgeobase/management/commands/ipgeobase_update.py
+++ django_ipgeobase/management/commands/ipgeobase_update.py
@@ -3,11 +3,11 @@ from django.core.management.base import NoArgsCommand, CommandError
 from django.db import connection, transaction
 from django_ipgeobase import conf
 from zipfile import ZipFile
-from urllib import urlopen
-from cStringIO import StringIO
-from django.core.mail import mail_admins
+import urllib2
 from django_ipgeobase import models
-
+from hashlib import md5
+from os import path
+import datetime
 
 DELETE_SQL = \
 """
@@ -17,123 +17,139 @@ DELETE FROM django_ipgeobase_ipgeobase
 INSERT_SQL = \
 """
 INSERT INTO django_ipgeobase_ipgeobase
-(ip_block, "start_ip", "end_ip", city, region, district, latitude, longitude)
-VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+(start_ip, end_ip, ip_block, country_id, city_id, region_id)
+VALUES (%s, %s, %s, %s, %s, %s)
 """
 
-ERROR_SUBJECT = u"Error of command ipgeobase_update"
-send_message = conf.IPGEOBASE_SEND_MESSAGE_FOR_ERRORS
-
-def get_url(url):
-    from hashlib import md5
-    from os import path
-    h = md5()
-    h.update(url)
-    fn = '/tmp/%s.txt' % h.hexdigest()
-    if path.exists(fn):
-        return open(fn, 'rb')
-    else:
-        content = urlopen(url).read()
-        f = open(fn, 'wb')
-        f.write(content)
-        f.close()
-        return StringIO(content)
-
+COUNTRIES = {'RU': u'Россия',
+             'UA': u'Украина'}
 
 class Command(NoArgsCommand):
 
-    def get_row(self, row, query_info):
-        record = {}
-        record.update(query_info.get('defaults', {}))
-        for i, field_name in enumerate(query_info['fields']):
-            if field_name is not None:
-                val = row[i].strip()
-                if val:
-                    record[field_name] = val
-        record['country'] = record['country'].upper()
-        return record
-
-    def get_contents(self, query_info):
-        buff = get_url(query_info['url'])
-        if query_info['format'] == 'zip':
-            zip_file = ZipFile(buff)
-            try:
-                content = zip_file.read(query_info['zip_filename'])
-            except KeyError:
-                raise CommandError("File %s in archive not found" % query_info['zip_filename'])
-            zip_file.close()
-        else:
-            content = buff.read()
-        buff.close()
-        return content
-
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
+        self._cache_existing_geo_objects()
+
+    def _cache_existing_geo_objects(self):
         self.countries = {}
         self.cities = {}
         self.regions = {}
 
-        for c in models.IPGeoBase_Country.objects.all():
-            self.countries[c.code.upper()] = c.code
-
-        for c in models.IPGeoBase_Region.objects.all():
-            self.regions[(c.country_id, c.name.lower())] = c.id
-
-        for c in models.IPGeoBase_City.objects.all():
-            self.cities[(c.country_id, c.region_id, c.name.lower())] = c.id
-
-    def preprocess_row(self, row):
-        row['country'] = row['country'].upper()
-        if row['country'] not in self.countries:
-            country = models.IPGeoBase_Country.objects.create(code=row['country'], name=row['country'])
+        for country in models.IPGeoBase_Country.objects.all():
             self.countries[country.code] = country.code
-            print "created country", country.name
-        row['country'] = self.countries[row['country']]
+        for region in models.IPGeoBase_Region.objects.all():
+            self.regions[(region.country_id, region.name.lower())] = region.id
+        for city in models.IPGeoBase_City.objects.all():
+            self.cities[int(city.id)] = int(city.id)
 
-        region = row['region'].lower()
-        region_key = (row['country'], region)
-        if region_key not in self.regions:
-            region = models.IPGeoBase_Region.objects.create(country_id=row['country'], name=row['region'])
-            self.regions[region_key] = region.id
-            print "created city", row['region']
-        row['region'] = self.regions[region_key]
-
-        city_key = (row['country'], row['region'], row['city'].lower())
-        if city_key not in self.cities:
-            city = models.IPGeoBase_City.objects.create(country_id=row['country'], region_id=row['region'], name=row['city'])
-            self.cities[city_key] = city.id
-            print "created city", row['city']
-        row['city'] = self.cities[city_key]
-        return row
-
-    @transaction.commit_on_success
     def handle_noargs(self, *args, **options):
-        database = []
-        print "Init done"
-        for query_info in conf.IPGEOBASE_COUNTRY_INFO:
-            content = self.get_contents(query_info)
-            for line in content.decode(query_info['coding']).split("\n"):
-                line = line.strip()
-                if not line:
-                    continue
+        print 'Starting IP Geobase update...'
+        ip_blocks, self.city_mapping = self._process_cidr()
+        self._process_city_data()
+        self._renew_ipblocks(ip_blocks)
 
-                row = self.get_row(line.split("\t"), query_info)
-                row = self.preprocess_row(row)
-                database.append(
-                    (row['start_ip'], row['end_ip'], row['city'], row['region'], row['country'], row['ip_block']),
-                )
+    def _process_cidr(self):
+        cidr_data = self._get_entries_from_zipfile(conf.IPGEOBASE_CIDR_INFO)
+        ip_blocks = []
+        city_mapping = {'country':{}, 'region':{}}
+        for line in cidr_data:
+            row = dict(zip(conf.IPGEOBASE_CIDR_INFO['fields'], line.split("\t")))
+            if row['city_id'].isdigit():
+                self._add_country_if_not_exists(row['country_id'])
+                city_mapping['country'][row['city_id']] = row['country_id']
+                ip_blocks.append(line.split("\t"))
+        return ip_blocks, city_mapping
+
+    def _process_city_data(self):
+        city_data = self._get_entries_from_zipfile(conf.IPGEOBASE_CITIES_INFO)
+        for line in city_data:
+            row = dict(zip(conf.IPGEOBASE_CITIES_INFO['fields'], line.split("\t")))
+            if row['city_id'] in self.city_mapping['country']:
+                country_id = self.city_mapping['country'][row['city_id']]
+            else:
+                continue
+            self._add_region_if_not_exists(country_id = country_id,
+                                           region_name = row['region_name'])
+            region_id = self._get_region_id(country_id, row['region_name'])
+            self.city_mapping['region'][row['city_id']] = region_id
+            self._add_city_if_not_exists(city_id = row['city_id'],
+                                         city_name = row['city_name'],
+                                         region_id = region_id,
+                                         country_id = country_id)
+
+    def _get_region_id(self, country_id, region_name):
+        key = (country_id, region_name.lower())
+        return self.regions[key]
+
+    def _get_entries_from_zipfile(self, config):
+        zip_file = self._get_remote_file_contents(config['url'])
+        content = self._get_unzipped_content(zip_file, config['filename'])
+        return [line.strip() for line in content.decode(config['encoding']).split("\n")
+                 if line.strip()]
+
+    def _add_country_if_not_exists(self, country_id):
+        if country_id not in self.countries:
+            country = models.IPGeoBase_Country.objects.create(code=country_id,
+                                                              name=COUNTRIES.get(country_id, country_id))
+            self.countries[country.code] = country.code
+            return country
+
+    def _add_region_if_not_exists(self, country_id, region_name):
+        region_key = (country_id, region_name.lower())
+        if region_key not in self.regions:
+            region = models.IPGeoBase_Region.objects.create(country_id=country_id, name=region_name)
+            self.regions[region_key] = region.id
+            return region
+
+    def _add_city_if_not_exists(self, city_id, city_name, region_id, country_id):
+        if int(city_id) not in self.cities:
+            city = models.IPGeoBase_City.objects.create(id=city_id, name=city_name,
+                                                        region_id=region_id,
+                                                        country_id=country_id)
+            self.cities[city_id] = city.id
+            return city_id
+
+    @transaction.commit_manually
+    def _renew_ipblocks(self, ipblocks):
+        print 'Updating CIDR...'
         try:
             cursor = connection.cursor()
-            cursor.execute("TRUNCATE TABLE django_ipgeobase_ipgeobase")
-            print cursor.executemany("""
-            INSERT INTO django_ipgeobase_ipgeobase
-            (start_ip, end_ip, city_id, region_id, country_id, ip_block)
-            VALUES (%s, %s, %s, %s, %s, %s )
-            """, database)
+            cursor.execute(DELETE_SQL)
+            for row in ipblocks:
+                row.append(self.city_mapping['region'][row[4]])
+            cursor.executemany(INSERT_SQL, ipblocks)
             transaction.commit()
-        except Exception, e:
+        except Exception as e:
             transaction.rollback()
-            message = "The data not updated:", e
-            if send_message:
-                mail_admins(subject=ERROR_SUBJECT, message=message)
-            raise CommandError(message)
+            print e
+            print 'Geo data not updated'
+
+    def _get_remote_file_contents(self, url):
+
+        def get_local_name(url):
+            now = datetime.datetime.now()
+            md5_url_hash = md5(url+now.strftime('%Y-%m-%d')).hexdigest()
+            return u'/tmp/{0}.zip'.format(md5_url_hash)
+
+        def download_file(url, local_name):
+            print 'Downloading {0}'.format(url)
+            remote_file = urllib2.urlopen(url)
+            output = open(local_name, 'wb')
+            output.write(remote_file.read())
+            output.close()
+
+        local_name = get_local_name(url)
+        if not path.exists(local_name):
+            download_file(url, local_name)
+        return open(local_name, 'rb')
+
+
+    def _get_unzipped_content(self, zip_file, extract_file):
+        zip = ZipFile(zip_file)
+        try:
+            content = zip.read(extract_file)
+        except KeyError:
+            raise CommandError("File %s not found in archive".format(extract_file))
+        zip.close()
+        print 'Unzipped {0} successfully!'.format(zip_file.name)
+        return content
