31bb0e2b46357d2488b83018b6c1c14b1f7ca158
Ilya Baryshev <baryshev@gmail.com>
2011-06-01 10:51:13 +0400

    Обновлён CHANGELOG и README Версия 0.1 в setup.py

--- setup.py
+++ setup.py
@@ -7,19 +7,19 @@ reload(sys).setdefaultencoding("UTF-8")
 
 setup(
     name='django-ipgeobase',
-    version='0.5.1',
-    author='Ivan Petukhov',
-    author_email='satels@gmail.com',
+    version='0.1.0',
+    author='Future Colors',
+    author_email='info@futurecolors.ru',
     packages=['django_ipgeobase', 'django_ipgeobase.management',
               'django_ipgeobase.management.commands'],
-    url='https://github.com/satels/django-ipgeobase',
-    download_url = 'https://github.com/satels/django-ipgeobase/zipball/master',
+    url='https://github.com/futurecolors/django-ipgeobase',
+    download_url = 'https://github.com/futurecolors/django-ipgeobase/zipball/master',
     license = 'MIT license',
     description = u'Приложение для работы с базой ipgeobase.ru.'.encode('utf8'),
     long_description = open('README').read().decode('utf8'),
 
     classifiers=(
-        'Development Status :: 1',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
