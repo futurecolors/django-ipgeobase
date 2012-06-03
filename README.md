f8ead21fc1ada4ef894c3e259688e300d37d7990
Ilya Baryshev <baryshev@gmail.com>
2012-06-03 23:27:25 +0400

    Не поддерживается в пользу django-geoip

---
+++ README.md
@@ -0,0 +1,3 @@
+h1. Disclaimer
+
+This app is no longer maintained, see https://github.com/futurecolors/django-geoip
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ reload(sys).setdefaultencoding("UTF-8")
 
 setup(
     name='django-ipgeobase',
-    version='0.1.0',
+    version='0.1.1',
     author='Future Colors',
     author_email='info@futurecolors.ru',
     packages=['django_ipgeobase', 'django_ipgeobase.management',
@@ -16,10 +16,10 @@ setup(
     download_url = 'https://github.com/futurecolors/django-ipgeobase/zipball/master',
     license = 'MIT license',
     description = u'Приложение для работы с базой ipgeobase.ru.'.encode('utf8'),
-    long_description = open('README').read().decode('utf8'),
+    long_description = open('docs/index.rst').read().decode('utf8'),
 
     classifiers=(
-        'Development Status :: 4 - Beta',
+        'Development Status :: 7 - Inactive',
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
